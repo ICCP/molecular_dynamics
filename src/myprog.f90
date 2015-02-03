3949c9d674c431d08539e0ddfd85bf44dfc6d567
bofo90 <f.agustin@gmail.com>
2015-02-03 15:10:31 -0500

    arreglar las posiciones ahora correctas

--- src/myprog.f90
+++ src/myprog.f90
@@ -4,42 +4,42 @@ program ArgonGas
 
   integer, parameter :: length = 3, particles = 32
   integer, parameter :: temperature = 300
-  real(8) :: position(length,32), velocity(length,32)
+  real(8) :: position(3,32), velocity(3,32)
 
 
   call initialize_position(position, length)
-  call initialize_velocity(velocity, length, particles)
+ ! call initialize_velocity(velocity, length, particles)
       
-  write(*,"(3E10.2)") position !3 integers of width 3
-  print *, "Velocidades"
+  write(*,"(3E10.2)") position !3 reals of width 10 and 2 decimals
+  print *, "Velocidades" 
   write(*,"(3E10.2)") velocity
 
 contains
 
   subroutine initialize_position(position, L)
 
-    real(8), intent(inout) :: position(3,32)
     integer, intent(in) :: L
+    real(8), intent(inout) :: position(3,32)
     integer :: i, j, k, n=1
 
-    do i = 1, L-1
-       do j = 1, L-1
-          do k = 1, L-1
-             position(1,n) = i            !First particle in origin
-             position(2,n) = j
-             position(3,n) = k
+    do i = 0, L-2
+       do j = 0, L-2
+          do k = 0, L-2
+             position(1,n) = i*sqrt(2.0)            !First particle in origin
+             position(2,n) = j*sqrt(2.0)
+             position(3,n) = k*sqrt(2.0)
              n = n+1
-             position(1,n) = i+0.5        !Second particle in face k=1
-             position(2,n) = j+0.5
-             position(3,n) = k
+             position(1,n) = i*sqrt(2.0)+1/sqrt(2.0)!Second particle in face k=1
+             position(2,n) = j*sqrt(2.0)+1/sqrt(2.0)
+             position(3,n) = k*sqrt(2.0)
              n = n+1
-             position(1,n) = i             !Third in face i=1
-             position(2,n) = j+0.5
-             position(3,n) = k+0.5
+             position(1,n) = i*sqrt(2.0)            !Third in face i=1
+             position(2,n) = j*sqrt(2.0)+1/sqrt(2.0)
+             position(3,n) = k*sqrt(2.0)+1/sqrt(2.0)
              n = n+1
-             position(1,n) = i+0.5        !Fourth in face j=1
-             position(2,n) = j
-             position(3,n) = k+0.5
+             position(1,n) = i*sqrt(2.0)+1/sqrt(2.0)!Fourth in face j=1
+             position(2,n) = j*sqrt(2.0)
+             position(3,n) = k*sqrt(2.0)+1/sqrt(2.0)
              n = n+1
           end do
        end do
@@ -56,16 +56,16 @@ contains
 
     do i = 1, L
        do j = 1, N
-          velocity(i,j) = gaussRandom()
+          !velocity(i,j) = gaussRandom()
        end do
     end do
 
   end subroutine initialize_velocity
 
-  real(8) function gaussRandom() result(rand_vel)
+ ! real(8) function gaussRandom() result(rand_vel)
 
 
-  end function gaussRandom
+ ! end function gaussRandom
  
 
 end program
