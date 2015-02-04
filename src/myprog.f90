0fedbfcbdc47d30e7305226c38a1c1eff5d5aa01
Gregorio <pontigre@msu.edu>
2015-02-04 11:06:40 -0500

    Connor - plplot

--- src/myprog.f90
+++ src/myprog.f90
@@ -1,15 +1,15 @@
 program ArgonGas
 
- ! use md_plot
+  use md_plot
   use initialization
   use force
   use posiciones
 
   implicit none
 
-  integer, parameter :: boxes = 2, particles = 32, mass = 1
+  integer, parameter :: boxes = 2, particles = 32
   integer, parameter :: temperature = 3
-  real(8) :: positions(3,particles), velocity(3,particles), forces(3,particles)
+  real(8) :: positions(3,32), velocity(3,32), forces(3,32)
   real(8) :: boxSize = boxes*2**(2.0/3), timestep = 1
 
   call initialize_position(positions, boxes)
@@ -21,9 +21,8 @@ program ArgonGas
   print *, "fuerzas" 
   write(*,"(3E10.2)") forces
 
-!  call md_plot_init(boxSize)
-!  call md_plot_points(positions)
-  
-!  call md_plot_close()
+  call md_plot_init(boxSize)
+  call md_plot_points(positions)
+  call md_plot_close()
 
 end program
