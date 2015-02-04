126f0e453475e4a27e69e5e72d163cbf1a53aed2
Gregorio <pontigre@msu.edu>
2015-02-04 10:20:42 -0500

    Connor again

--- src/posiciones.f90
+++ src/posiciones.f90
@@ -2,23 +2,24 @@ module change_positions
   
   private
 
-  public change_positiones
+  public change_posiciones
 
 contains
   
-  subroutine change_positions(forces, positions, velocity, N, boxes, timestep)
+  subroutine change_posiciones(forces, positions, velocity, N, boxes, timestep)
 
     real(8), intent(in) :: forces(3,32)
     real(8), intent(inout) :: positions(3,32)
     real(8), intent(in) :: velocity(3,32)
-    integer, intent(in) :: N, boxes, timestep, mass
+    integer, intent(in) :: N, boxes, timestep
 
     real(8) :: x, y, z
     integer :: i, j
 
     do i = 1, N
        do j = 1, 3
-       positions(j,i) = positions(j,i) + velocity(j,i)*timestep + (0.5)*forces(j,i)*(1/mass)*(timestep)**2
+       positions(j,i) = positions(j,i) + velocity(j,i)*timestep + (0.5)*forces(j,i)*(timestep)**2
+       end do
     end do 
 
   end subroutine
