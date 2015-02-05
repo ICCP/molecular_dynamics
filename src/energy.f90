4653965b5eb4f1ae76b144ccde2da26b90407616
Gregorio <pontigre@msu.edu>
2015-02-05 10:11:04 -0500

    anda cagar

--- src/energy.f90
+++ src/energy.f90
@@ -16,7 +16,7 @@ contains
     ener_kin = 0
 
     do i = 1, particles
-       ener_kin = ener_kin + norm2(velocity(:,i))**2
+       ener_kin = ener_kin +(0.5)*norm2(velocity(:,i))**2
     end do
 
   end subroutine calculate_kin_energy
