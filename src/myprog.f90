03eb903afd9f942f396f4eca192b4a4fc76bb8fc
bofo90 <f.agustin@gmail.com>
2015-02-12 00:10:13 +0100

    arreglo del pair correlation (se necesitaba inizializar)

--- src/myprog.f90
+++ src/myprog.f90
@@ -7,12 +7,12 @@ program ArgonGas
 
   implicit none
 
-  integer, parameter :: particles = 864
-  real(8), parameter :: density = 1
-  real(8), parameter :: temp_target = 1
+  integer, parameter :: particles = 2048
+  real(8), parameter :: density = 0.5
+  real(8), parameter :: temp_target = 3
 
   integer, parameter :: boxes = nint((particles/4)**(1.0/3))
-  integer, parameter :: number_timesteps = 2000
+  integer, parameter :: number_timesteps = 5000
   real(8), parameter :: time_step = 0.004
   real(8), parameter :: length = boxes*(4.0/density)**(1.0/3)
 
@@ -28,12 +28,7 @@ program ArgonGas
   call initialize_velocity(velocity, particles)
   call setting_cero_velocity(velocity, particles)
   forces = 0._8
-
-!!$  call initplot ('lightblue', 800,800, 'out.ps', 1)
-!!$  call Framing (0._8, 0._8, length, length)
-!!$  call putstopbutton()
-
-  call calculate_force(forces, particles, position, length, ener_pot, pair_corre, flag, virial_function)
+  pair_corre = 0d0
 
   open (unit=11,file='ener_kin_data.txt')
   open (unit=12,file='ener_pot_data.txt')
@@ -42,12 +37,18 @@ program ArgonGas
   open (unit=15,file='pair_corre_data.txt')
   open (unit=16,file='pressure_data.txt')
 
+  call initplot ('lightblue', 800,800, 'out.ps', 1)
+  call Framing (0._8, 0._8, length, length)
+  call putstopbutton()
+
+  call calculate_force(forces, particles, position, length, ener_pot, pair_corre, flag, virial_function)
+
   do i = 0, number_timesteps
      velocity = velocity + forces*time_step/2.0
      position = modulo(position+velocity*time_step, length)
      call calculate_force(forces, particles, position, length, ener_pot, pair_corre, flag, virial_function)
      velocity = velocity + forces*time_step/2.0
-!!$     call plot_particles(position, particles)
+     call plot_particles(position, particles)
      ener_kin = sum(velocity**2*0.5)
      call thermostat(particles, temp_target, velocity, i)
      pressure = particles*temp_target/(length**3) + (virial_function/(3*particles*(length**3)))
@@ -62,10 +63,14 @@ program ArgonGas
      write(16,*) i, pressure
   end do
  
+  pair_corre = pair_corre*2.0*(length**3)
+  pair_corre = pair_corre/(particles*(particles-1))
+  pair_corre = pair_corre/(number_timesteps-700)
   do j = 1, nint(length*sqrt(3.0)*0.5/0.01)
-     write(15,*) j/100.0, pair_corre(j)*2.0*(length**3)/(particles*(particles-1)*(number_timesteps-700))
+     write(15,*) j/100.0, pair_corre(j)
   end do
-  
+
+  print *, "this is the end...."  
 !!$  !Proove to velocities equal to cero
 !!$  v_test(:)=0
 !!$  do i=1, particles
@@ -73,17 +78,17 @@ program ArgonGas
 !!$  end do
 !!$  print*, v_test
 
-!!$  call EndPlot()
+  call EndPlot()
 
-!!$contains
-!!$  
-!!$  subroutine plot_particles(pos, n)
-!!$    integer :: i, n
-!!$    real(8) :: pos(3,n)
-!!$    
-!!$    do i= 1, n
-!!$       call setpoint(pos(1,i), pos(2,i))
-!!$    end do
-!!$  end subroutine plot_particles
+contains
+  
+  subroutine plot_particles(pos, n)
+    integer :: i, n
+    real(8) :: pos(3,n)
+    
+    do i= 1, n
+       call setpoint(pos(1,i), pos(2,i))
+    end do
+  end subroutine plot_particles
 
 end program ArgonGas
