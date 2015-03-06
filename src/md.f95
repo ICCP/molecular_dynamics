74ea758331301d2739a0f01099cfc088042c63af
Scott O'Connor <oconn220@msu.edu>
2015-03-06 10:43:46 -0500

    Added inital lattice position functionality

--- src/md.f95
+++ src/md.f95
@@ -5,26 +5,46 @@ program md
   integer :: nxcells,nycells,nzcells,ncells,ppc,nprtl,prtlnum
   real(8),dimension(:,:),allocatable :: pos,vel
   real(8),dimension(4,3) :: fcc
-  integer :: ii,jj,kk
+  integer :: ii,jj,kk,ll
+  real(8) :: xcellscl,ycellscl,zcellscl
+
   !Program Settings
-  nxcells = 10 !Number of cells in the X directions
-  nycells = 10 !Number of cells in the Y directions
-  nzcells = 10 !Number of cells in the Z direction
+  nxcells = 10   !Number of cells in the X directions
+  nycells = 10   !Number of cells in the Y directions
+  nzcells = 10   !Number of cells in the Z direction
+  xcellscl = 1.0 !Width of cell in X direction
+  ycellscl = 1.0 !Width of cell in y direction
+  zcellscl = 1.0 !Width of cell in z direction
   ncells = nxcells*nycells*nzcells !Number of total boxes
   ppc = 4      !particle per cell
   nprtl = ncells*ppc !number of particles
+
+  !FCC Cordinates
   fcc(1,:) = (/0.0,0.0,0.0/)
   fcc(2,:) = (/0.0,0.5,0.5/)
   fcc(3,:) = (/0.5,0.5,0.0 /)
   fcc(4,:) = (/0.5,0.0,0.5 /)
   allocate(pos(nprtl,3))
-
-  do ii = 1,ncells
-    do jj = 1,ppc
-        pos(ii,1) = fcc(jj,1) + mod(ii,nxcells)
-        pos(ii,2) = fcc(jj,2) + 
-        pos(ii,3) = fcc(jj,3) + 
+  
+  !create lattice
+  do ii = 1,nzcells
+    do jj = 1,nycells
+      do kk = 1,nxcells
+        do ll = 1,ppc
+        prtlnum = (ii-1)*nycells*nxcells*ppc+(jj-1)*nxcells*ppc+(kk-1)*ppc + ll
+        pos(prtlnum,1) = fcc(ll,1) + (ii-1)*xcellscl
+        pos(prtlnum,2) = fcc(ll,2) + (jj-1)*ycellscl
+        pos(prtlnum,3) = fcc(ll,3) + (kk-1)*zcellscl
+        end do 
+      end do 
     end do
   end do 
+
+  !write lattice to file
+  open (unit = 1, file = 'pos.out', status = 'unknown')
+  do ii = 1,nprtl
+    write (1,*),ii,pos(ii,:)
+  end do
+
 
 end program
