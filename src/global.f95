a80d4d4f79a5712360c648813e9e0be55e4d6633
Scott O'Connor <sconn220@gmail.com>
2015-03-15 20:57:18 -0400

    Added force function and verified

--- src/global.f95
+++ src/global.f95
@@ -10,16 +10,15 @@ integer :: prtlnum                    !Particle Number
 real(8) :: xcellscl,ycellscl,zcellscl !Width of fcc cell
 
 !arrays that hold the positions and velocity of each particle
-real(8), dimension(:,:),allocatable :: pos,vel,accel
-real(8), dimension(3) :: rand_vel = 0
+real(8), dimension(:,:,:),allocatable :: pos,vel,accel   
+real(8), dimension(3) :: rand_vel = 0           !varible for assigning random velocities
+real(8), dimension(3) :: prtl_accel = 0         !acceration on an individual particle
+real(8), dimension(3) :: prtl_force_lj = 0      !force on an individual particle
 
+real(8) :: dt !timestep
+real(8) :: NT !Number of timestep
 !Array the hold the position of particles in the unit fcc cell
 real(8), dimension(4,3) :: fcc
-
-!Force and aceleration
-real(8), dimension(3) :: prtl_accel = 0
-real(8), dimension(3) :: prtl_force_lj = 0
-
 
 integer :: ii,jj,kk,ll                !Indexer Variables
 
--- src/md.f95
+++ src/md.f95
@@ -20,16 +20,23 @@ program md
   fcc(2,:) = (/0.0,0.5,0.5/)
   fcc(3,:) = (/0.5,0.5,0.0 /)
   fcc(4,:) = (/0.5,0.0,0.5 /)
-  
-  allocate(pos(nprtl,3))   !allocate position
-  allocate(vel(nprtl,3))   !allocate velocity
-  allocate(accel(nprtl,3)) !allocate acceration
+ 
+  NT = 1
+
+  allocate(pos(nprtl,3,1))   !allocate position
+  allocate(vel(nprtl,3,1))   !allocate velocity
+  allocate(accel(nprtl,3,1)) !allocate acceration
 
   call bld_lattice  !Create inital position of gas particles
   call writepos     !write positions to files
   call writevel     !write velocities to files
 
 
+
+  call force_lj(fcc(1,:),fcc(2,:),prtl_force_lj)
+
+  print*,'prtl_force_lj',prtl_force_lj
+  print*,'Norm force', norm2(prtl_force_lj)
 end program
 
 subroutine bld_lattice !{{{
@@ -50,16 +57,16 @@ subroutine bld_lattice !{{{
         prtlnum = (ii-1)*nycells*nxcells*ppc+(jj-1)*nxcells*ppc+(kk-1)*ppc + ll
         
         !Set inital position
-        pos(prtlnum,1) = fcc(ll,1) + (ii-1)*xcellscl
-        pos(prtlnum,2) = fcc(ll,2) + (jj-1)*ycellscl
-        pos(prtlnum,3) = fcc(ll,3) + (kk-1)*zcellscl
+        pos(prtlnum,1,1) = fcc(ll,1) + (ii-1)*xcellscl
+        pos(prtlnum,2,1) = fcc(ll,2) + (jj-1)*ycellscl
+        pos(prtlnum,3,1) = fcc(ll,3) + (kk-1)*zcellscl
   
         !Create and scale random velocities
         call random_number(rand_vel)
         call scalerand(rand_vel)
 
         !Assign random velocities
-        vel(prtlnum,1:3) = rand_vel
+        vel(prtlnum,1:3,1) = rand_vel
         end do 
       end do 
     end do
@@ -76,7 +83,7 @@ subroutine writepos !{{{
 
   open (unit = 1, file = 'pos.out', status = 'unknown')
   do ii = 1,nprtl
-    write (1,*),ii,pos(ii,:)
+    write (1,*),ii,pos(ii,:,1)
   end do
 end subroutine !}}}
 
@@ -89,7 +96,7 @@ subroutine writevel!{{{
 
   open (unit = 2, file = 'vel.out', status = 'unknown')
   do ii = 1,nprtl
-    write (2,*),ii,vel(ii,:)
+    write (2,*),ii,vel(ii,:,1)
   end do
 
 end subroutine !}}}
@@ -105,3 +112,44 @@ subroutine scalerand(randvel)  !{{{
   randvel = 2*randvel-1
   
 end subroutine  !}}}
+
+subroutine force_lj(pos1,pos2,force) !{{{
+  
+  !Calculates the force due to the Lenard Jones Potiential 
+  !Input: pos1,pos2- position of the first and second particle
+  
+  use global
+
+  implicit none
+  
+  !input variabels 
+  real(8),dimension(3),intent(in) :: pos1,pos2  !position of particle 1 and 2
+  real(8),dimension(3),intent(out) :: force      !force between particles
+  
+  !Internal Variable
+  real(8),dimension(3) :: r ,r_norm  !Distance Between Radius
+  real(8) :: force_mag               !Magnitude of the Force
+
+  r = pos1 - pos2             !Finding the Distance Between the Points
+  r_norm = r/norm2(r)         !Normalized Distance
+
+  !Lenard Jones Potential 
+  force_mag = 24.d0*((2.d0/(norm2(r))**13)+(-1.d0/(norm2(r))**7))
+
+  !Direction Force
+  force = r_norm * force_mag
+
+end subroutine !}}}
+
+
+
+
+
+
+
+
+
+
+
+
+
